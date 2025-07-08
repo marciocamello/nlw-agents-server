7d08bb5f8863782043153c4d4517c5276f8e952e
Marcio Vale <mac3designer@gmail.com>
2025-07-08 20:56:36 +0100

    feat: add rooms listing

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+    PORT: z.coerce.number().default(3333),
+    DATABASE_URL: z.string().url().startsWith('postgres://'),
+});
+
+export const env = envSchema.parse(process.env);
+export type Env = z.infer<typeof envSchema>;
